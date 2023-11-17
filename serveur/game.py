7e8ef0b5254b0d921a30fdfcf081449388387a07
amazuc <arthur.mazuc@hotmail.fr>
2023-11-17 11:34:29 +0100

    print quit

--- serveur/game.py
+++ serveur/game.py
@@ -55,7 +55,7 @@ class Game():
 
     def signal_handler(self, signal, data):
         self.listener.close()
-        self.echo("QUIT")
+        self.print("QUIT")
 
     def initJoueur(self, data) :
         parts = data.split(": ", 1)
